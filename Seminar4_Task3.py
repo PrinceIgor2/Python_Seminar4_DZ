29cd39f52051d520ef90f2aa5702d57d9d4a91cd
Igor <kugimn@list.ru>
2022-12-05 16:20:37 +0300

    4+

--- Seminar4_Task3.py
+++ Seminar4_Task3.py
@@ -1,4 +1,6 @@
-# 3 - В файле, содержащем фамилии студентов и их оценки, изменить на буквы в верхнем регистре тех студентов, которые имеют средний балл более «4».
+# 3 - В файле, содержащем фамилии студентов и их оценки, изменить на буквы в верхнем регистре тех студентов, 
+# которые имеют средний балл более «4».
+
 # Нужно перезаписать файл.
 # Пример:
 # Ангела Меркель 5
@@ -11,3 +13,41 @@
 # ЭНАКИН СКАЙУОКЕР 5
 # Фредди Меркури 3
 # Александр Пушкин 4
+
+
+# with open('file.txt', 'a', encoding='utf-8') as data:
+#     data.write('Ангела Меркель 5\n')
+#     data.write('Энакин Скайуокер 5\n')
+#     data.write('Фредди Меркури 3\n')
+#     data.write('Александр Пушкин 4\n')
+
+#     # for i in data:
+#     #     last_name, first_name, points = i.split()
+#     # if int(points) > 4:
+#     #     print(f'{last_name.upper()} {first_name.upper()}')
+
+# data.close()
+
+
+
+# path = 'file.txt'
+
+# with open(path, 'w', encoding='utf-8') as file:
+#     pupils = path.write().split()
+# for pupil in pupils:
+#     last_name, first_name, points = pupil.split()
+# if int(points) > 4:
+#     print(f'{last_name} {first_name}')
+   
+# path.close()
+
+
+n = int(input("Введите через пробел: "))
+nameList = []
+for i in range(n):
+    line1 = list(input().split())
+    nameList.append((line1[1], line1[0]))
+    nameList.sort(key=lambda x: -int(x[0]))
+for i in nameList:
+    print(i[1])
+ 
--- Seminar4_Task4.py
+++ Seminar4_Task4.py
@@ -4,3 +4,44 @@
 # Сдвиг часто называют ключом шифрования.
 # Ваша задача - написать функцию, которая записывает в файл шифрованный текст, а также функцию,
 #  которая спрашивает ключ, считывает текст и дешифровывает его.
+
+
+alphabet_ru = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
+
+def shift(text, symbols, n):
+    index = symbols.find(text)
+    if index + n < len(symbols):
+        return symbols[index + n]
+    else:
+        return symbols[(index + n) % len(symbols)]
+
+    
+
+def back_shift(text, symbols, n):
+    index = symbols.find(text)
+    if index - n >= 0:
+        return symbols[index - n]
+    else:        
+        return symbols[(index - n) % len(symbols)]
+
+    
+
+def encrypt(text, n = 2):
+    res = ""
+    for i in range(0, len(text)): 
+        res += shift(text[i], alphabet_ru, n)
+    return res
+
+
+def decrypt (text, n = 2):
+    res = ""
+                   
+    for i in range(0, len(text)):
+        res += back_shift(text[i], alphabet_ru, n)
+    return res
+
+str = encrypt(input("Введите слово для кодирования: "))
+print(f'Закодированный вариант: {str}')
+decrypt_str = decrypt(str)
+print(f'Раскодированный вариант: {decrypt_str}')
